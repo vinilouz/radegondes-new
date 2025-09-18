daa876c0fbce3b9ca55c07e46e150854c88888f6
Vini Louz <vinilouz@gmail.com>
2025-09-18 20:51:09 -0300

    fix vite allow urls

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,23 +3,35 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-	plugins: [tailwindcss(), tanstackRouter({}), react(), VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "radegondes",
-        short_name: "radegondes",
-        description: "radegondes - PWA Application",
-        theme_color: "#0c0c0c",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const allowedHosts = env.ALLOWED_URL ? [env.ALLOWED_URL] : [];
+
+  return {
+    plugins: [
+      tailwindcss(),
+      tanstackRouter({}),
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest: {
+          name: "radegondes",
+          short_name: "radegondes",
+          description: "radegondes - PWA Application",
+          theme_color: "#0c0c0c",
+        },
+        pwaAssets: { disabled: false, config: true },
+        devOptions: { enabled: true },
+      })
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
-      pwaAssets: { disabled: false, config: true },
-      devOptions: { enabled: true },
-    })],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+    },
+    server: { allowedHosts },
+    preview: { allowedHosts }
+  };
 });
